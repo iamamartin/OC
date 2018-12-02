15756b2838cb8d30ac3a95ad674ec1727aaa8971
Anthony Martin <iamamartin@yahoo.com>
2018-12-02 06:12:47 -0500

    Bootstrap for nav

--- hello_world/app/assets/javascripts/application.js
+++ hello_world/app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Added jquery and bootstrap for nav
+//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require bootstrap-sprockets
